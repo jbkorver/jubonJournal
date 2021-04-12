26e4b2b3366916389a3f535fa0df72b5cd7c5c0a
jbkorver <32282155+jbkorver@users.noreply.github.com>
2021-04-12 16:31:54 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Jubon.biz</title>
+<link rel="stylesheet" type="text/css" media="screen" href="../css/jubon.css" />
+<script type="text/javascript">
+<!--
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}
+function MM_jumpMenuGo(objId,targ,restore){ //v9.0
+  var selObj = null;  with (document) { 
+  if (getElementById) selObj = getElementById(objId);
+  if (selObj) eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0; }
+}
+//-->
+</script>
+</head>
+ <script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-919812-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+      
+<body>
+	<div id="container">
+   	<?php include("header.php");?>
+    <div id="space30"></div>
+	<div id="directoryHeader">Jubon Journal</div>		
+
+        <div class="clearfloat"></div>
+        
+        <div id="description">
+           <div id="columnLeft">
+            <h2>Jubon Journal Archive</h2>
+            <ul>
+            	<li><a href="vol11Iss11.html">Vol 11, Issue 11</a> - November 24, 2019</li>
+            	<li><a href="vol11Iss10.html">Vol 11, Issue 10</a> - October 24, 2019</li>
+            	<li><a href="vol11Iss9.html">Vol 11, Issue 9</a> - September 24, 2019</li>
+            	<li><a href="vol11Iss8.html">Vol 11, Issue 8</a> - August 24, 2019</li>
+            	<li><a href="vol11Iss7.html">Vol 11, Issue 7</a> - July 24, 2019</li>
+            	<li><a href="vol11Iss6.html">Vol 11, Issue 6</a> - June 24, 2019</li>
+            	<li><a href="vol11Iss5.html">Vol 11, Issue 5</a> - May 24, 2019</li>
+            	<li><a href="vol_11_iss_4.html">Vol 11, Issue 4</a> - April 24, 2019</li>
+            	<li><a href="vol11Iss3.html">Vol 11, Issue 3</a> - March 24, 2019</li>
+            	<li><a href="vol11Iss2.html">Vol 11, Issue 2</a> - February 24, 2019</li>
+            	<li><a href="vol11Iss1.html">Vol 11, Issue 1</a> - January 24, 2019</li>
+            	<li><a href="vol10Iss12.html">Vol 10, Issue 12</a> - December 24, 2018</li>
+            	<li><a href="vol10Iss11.html">Vol 10, Issue 11</a> - November 24, 2018</li>
+            	<li><a href="vol10Iss10.html">Vol 10, Issue 10</a> - October 24, 2018</li>
+            	<li><a href="vol10Iss9.html">Vol 10, Issue 9</a> - September 24, 2018</li>
+            	<li><a href="vol10Iss8.html">Vol 10, Issue 8</a> - August 24, 2018</li>
+            	<li><a href="vol10Iss7.html">Vol 10, Issue 7</a> - July 24, 2018</li>
+            	<li><a href="vol10Iss6.html">Vol 10, Issue 6</a> - June 24, 2018</li>
+            	<li><a href="vol10Iss5.html">Vol 10, Issue 5</a> - May 24, 2018</li>
+            	<li><a href="vol10Iss4.html">Vol 10, Issue 4</a> - April 24, 2018</li>
+            	<li><a href="vol10Iss3.html">Vol 10, Issue 3</a> - March 24, 2018</li>
+            	<li><a href="vol10Iss2.html">Vol 10, Issue 2</a> - February 24, 2018</li>
+            	<li><a href="vol10Iss1.html">Vol 10, Issue 1</a> - January 24, 2018</li>
+            	<li><a href="vol9Iss12.html">Vol 9, Issue 12</a> - December 24, 2017</li>
+            	<li><a href="vol9Iss11.html">Vol 9, Issue 11</a> - November 24, 2017</li>
+            	<li><a href="vol9Iss10.html">Vol 9, Issue 10</a> - October 24, 2017</li>
+            	<li><a href="vol9Iss9.html">Vol 9, Issue 9</a> - September 24, 2017</li>
+            	<li><a href="vol9Iss8.html">Vol 9, Issue 8</a> - August 24, 2017</li>
+            	<li><a href="vol9Iss7.html">Vol 9, Issue 7</a> - July 24, 2017</li>
+            	<li><a href="vol9Iss6.html">Vol 9, Issue 6</a> - June 24, 2017</li>
+            	<li><a href="vol9Iss5.html">Vol 9, Issue 5</a> - May 24, 2017</li>
+            	<li><a href="vol9Iss4.html">Vol 9, Issue 4</a> - April 24, 2017</li>
+            	<li><a href="vol9Iss3.html">Vol 9, Issue 3</a> - March 24, 2017</li>
+            	<li><a href="vol9Iss2.html">Vol 9, Issue 2</a> - February 24, 2017</li>
+            	<li><a href="vol9Iss1.html">Vol 9, Issue 1</a> - January 24, 2017</li>
+            	<li><a href="vol8Iss12.html">Vol 8, Issue 12</a> - December 24, 2016</li>
+            	<li><a href="vol8Iss11.html">Vol 8, Issue 11</a> - November 24, 2016</li>
+            	<li><a href="vol8Iss10.html">Vol 8, Issue 10</a> - October 24, 2016</li>
+            	<li><a href="vol8Iss9.html">Vol 8, Issue 9</a> - September 24, 2016</li>
+            	<li><a href="vol8Iss8.html">Vol 8, Issue 8</a> - August 24, 2016</li>
+            	<li><a href="vol8Iss7.html">Vol 8, Issue 7</a> - July 24, 2016</li>
+            	<li><a href="vol8Iss6.html">Vol 8, Issue 6</a> - June 24, 2016</li>
+            	<li><a href="vol8Iss5.html">Vol 8, Issue 5</a> - May 24, 2016</li>
+            	<li><a href="vol8Iss4.html">Vol 8, Issue 4</a> - April 24, 2016</li>
+            	<li><a href="vol8Iss3.html">Vol 8, Issue 3</a> - March 24, 2016</li>
+            	<li><a href="vol8Iss2.html">Vol 8, Issue 2</a> - February 24, 2016</li>
+            	<li><a href="vol8Iss1.html">Vol 8, Issue 1</a> - January 24, 2016</li>
+            	<li><a href="vol7Iss12.html">Vol 7, Issue 12</a> - December 24, 2015</li>
+            	<li><a href="vol7Iss11.html">Vol 7, Issue 11</a> - November 24, 2015</li>
+            	<li><a href="vol7Iss10.html">Vol 7, Issue 10</a> - October 24, 2015</li>
+            	<li><a href="vol7Iss9.html">Vol 7, Issue 9</a> - September 24, 2015</li>
+            	<li><a href="vol7Iss8.html">Vol 7, Issue 8</a> - August 28, 2015</li>
+            	<li><a href="vol7Iss7.html">Vol 7, Issue 7</a> - July 28, 2015</li>
+            	<li><a href="vol7Iss6.html">Vol 7, Issue 6</a> - June 24, 2015</li>
+            	<li><a href="vol7Iss5.html">Vol 7, Issue 5</a> - May 24, 2015</li>
+            	<li><a href="vol7Iss4.html">Vol 7, Issue 4</a> - April 24, 2015</li>
+            	<li><a href="vol7Iss3.html">Vol 7, Issue 3</a> - March 24, 2015</li>
+            	<li><a href="vol7Iss2.html">Vol 7, Issue 2</a> - February 24, 2015</li>
+            	<li><a href="vol7Iss1.html">Vol 7, Issue 1</a> - January 24, 2015</li>
+            	<li><a href="vol6Iss12.html">Vol 6, Issue 12</a> - December 24, 2014</li>
+            	<li><a href="vol6Iss11.html">Vol 6, Issue 11</a> - November 24, 2014</li>
+            	<li><a href="vol6Iss10.html">Vol 6, Issue 10</a> - October 24, 2014</li>
+            	<li><a href="vol6Iss9.html">Vol 6, Issue 9</a> - September 24, 2014</li>
+            	<li><a href="vol6Iss8.html">Vol 6, Issue 8</a> - August 24, 2014</li>
+            	<li><a href="vol6Iss7.html">Vol 6, Issue 7</a> - July 24, 2014</li>
+            	<li><a href="vol6Iss6.html">Vol 6, Issue 6</a> - June 24, 2014</li>
+            	<li><a href="vol6Iss5.html">Vol 6, Issue 5</a> - May 24, 2014</li>
+            	<li><a href="vol6Iss4.html">Vol 6, Issue 4</a> - April 24, 2014</li>
+            	<li><a href="vol6Iss3.html">Vol 6, Issue 3</a> - March 24, 2014</li>
+            	<li><a href="vol6Iss2.html">Vol 6, Issue 2</a> - February 24, 2014</li>
+            	<li><a href="vol6Iss1.html">Vol 6, Issue 1</a> - January 24, 2014</li>
+            	<li><a href="vol5Iss12.html">Vol 5, Issue 12</a> - December 24, 2013</li>
+            	<li><a href="vol5Iss11.html">Vol 5, Issue 11</a> - November 24, 2013</li>
+            	<li><a href="vol5Iss10.html">Vol 5, Issue 10</a> - October 24, 2013</li>
+            	<li><a href="vol5Iss9.html">Vol 5, Issue 9</a> - September 24, 2013</li>
+            	<li><a href="vol5Iss8.html">Vol 5, Issue 8</a> - August 24, 2013</li>
+            	<li><a href="vol5Iss7.html">Vol 5, Issue 7</a> - July 24, 2013</li>
+            	<li><a href="vol5Iss6.html">Vol 5, Issue 6</a> - June 24, 2013</li>
+            	<li><a href="vol5Iss5.html">Vol 5, Issue 5</a> - May 24, 2013</li>
+            	<li><a href="vol5Iss4.html">Vol 5, Issue 4</a> - April 24, 2013</li>
+            	<li><a href="vol5Iss3.html">Vol 5, Issue 3</a> - March 24, 2013</li>
+            	<li><a href="vol5Iss2.html">Vol 5, Issue 2</a> - February 24, 2013</li>
+            	<li><a href="vol5Iss1.html">Vol 5, Issue 1</a> - January 24, 2013</li>
+                <li><a href="vol4Iss12.html">Vol 4, Issue 12</a> - December 24, 2012</li>
+            	<li><a href="vol4Iss11.html">Vol 4, Issue 11</a> - November 24, 2012</li>
+            	<li><a href="vol4Iss10.html">Vol 4, Issue 10</a> - October 24, 2012</li>
+            	<li><a href="vol4Iss9.html">Vol 4, Issue 9</a> - September 24, 2012</li>
+            	<li><a href="vol4Iss8.html">Vol 4, Issue 8</a> - August 24, 2012</li>
+            	<li><a href="vol4Iss7.html">Vol 4, Issue 7</a> - July 24, 2012</li>
+            	<li><a href="vol4Iss6.html">Vol 4, Issue 6</a> - June 24, 2012</li>
+            	<li><a href="vol4Iss5.html">Vol 4, Issue 5</a> - May 24, 2012</li>
+            	<li><a href="vol4Iss4.html">Vol 4, Issue 4</a> - April 24, 2012</li>
+            	<li><a href="vol4Iss3.html">Vol 4, Issue 3</a> - March 24, 2012</li>
+            	<li><a href="vol4Iss2.html">Vol 4, Issue 2</a> - February 24, 2012</li>
+            	<li><a href="vol4Iss1.html">Vol 4, Issue 1</a> - January 24, 2012</li>
+            	<li><a href="vol3Iss12.html">Vol 3, Issue 12</a> - December 24, 2011</li>
+            	<li><a href="vol3Iss11.html">Vol 3, Issue 11</a> - November 24, 2011</li>
+            	<li><a href="vol3Iss10.html">Vol 3, Issue 10</a> - October 24, 2011</li>
+            	<li><a href="vol3Iss9.html">Vol 3, Issue 9</a> - September 24, 2011</li>
+            	<li><a href="vol3Iss8.html">Vol 3, Issue 8</a> - August 24, 2011</li>
+            	<li><a href="vol3Iss7.html">Vol 3, Issue 7</a> - July 24, 2011</li>
+            	<li><a href="vol3Iss6.html">Vol 3, Issue 6</a> - June 24, 2011</li>
+            	<li><a href="vol3Iss5.html">Vol 3, Issue 5</a> - May 24, 2011</li>
+            	<li><a href="vol3Iss4.html">Vol 3, Issue 4</a> - April 24, 2011</li>
+            	<li><a href="vol3Iss3.html">Vol 3, Issue 3</a> - March 24, 2011</li>
+            	<li><a href="vol3Iss1.html">Vol 3, Issue 2</a> - February 24, 2011</li>
+            	<li><a href="vol3Iss1Original.html">Vol 3, Issue 1</a> - January 24, 2011</li>
+            	<li><a href="vol2Iss12.html">Vol 2, Issue 12</a> - December 24, 2010</li>
+            	<li><a href="vol2Iss11.html">Vol 2, Issue 11</a> - November 24, 2010</li>
+            	<li><a href="vol2Iss10.html">Vol 2, Issue 10</a> - October 24, 2010</li>
+            	<li><a href="vol2Iss9.html">Vol 2, Issue 9</a> - September 24, 2010</li>
+            	<li><a href="vol2Iss8.html">Vol 2, Issue 8</a> - August 24, 2010</li>
+            	<li><a href="vol2Iss7.html">Vol 2, Issue 7</a> - July 24, 2010</li>
+            	<li><a href="vol2Iss6.html">Vol 2, Issue 6</a> - June 24, 2010</li>
+            	<li><a href="vol2Iss5.html">Vol 2, Issue 5</a> - May 24, 2010</li>
+            	<li><a href="vol2Iss4.html">Vol 2, Issue 4</a> - April 24, 2010</li>
+            	<li><a href="vol2Iss3.html">Vol 2, Issue 3</a> - March 24, 2010</li>
+                <li><a href="vol2Iss2.html">Vol 2, Issue 2</a> - February 24, 2010</li>
+                <li><a href="vol2Iss1.html">Vol 2, Issue 1</a> - January 25, 2010</li>                
+               	<li><a href="vol1Iss9Save.html">Vol 1, Issue 9</a> - December 24, 2009</li>                
+                <li><a href="vol1Iss8.html">Vol 1, Issue 8</a> - November 24, 2009</li>
+                <li><a href="vol1Iss7new.html">Vol 1, Issue 7</a> - October 24, 2009</li>                
+            	<li><a href="vol1Iss6.html">Vol 1, Issue 6</a> - September 24, 2009</li>            	
+            	<li><a href="vol1Iss5.html">Vol 1, Issue 5</a> - August 24, 2009</li>
+            	<li><a href="vol1Iss4save.html">Vol 1, Issue 4</a> - July 24, 2009</li>
+            	<li><a href="vol1Iss3.html">Vol 1, Issue 3</a> - June 24, 2009</li>
+            	<li><a href="052409.html">Vol 1, Issue 2</a> - May 24, 2009</li>
+            	<li><a href="042409.html">Vol 1, Issue 1</a> - April 24, 2009</li>
+            </ul>
+            
+          </div><!--cityChooser-->
+           </div>
+           <div id="columnRight">
+             <p>Jubon has started a monthly email journal that will document new features, give helpful hints on how to use the Jubon maps, and showcase businesses on Jubon.  If you would like to receive the journal, sign up <a href="http://jubon.biz/notices.php">here</a>.</p>
+          </div>
+      </div><!--description-->
+    </div><!--container-->
+    <div id="space30"></div>
+	<?php include("footer.php");?>
+</body>
+</html>
